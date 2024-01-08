218ef1ef729ed37a4eb61c4af7e04819dc8723e6
VinayakD <135404363+vinayakDTP@users.noreply.github.com>
2024-01-08 04:33:48 +0000

    Add files via upload

---
+++ Odom/test.py
@@ -0,0 +1,21 @@
+import secrets
+import string
+
+def generate_password():
+    strength = input("How strong do you want the password to be? (weak, strong, or very strong): ").lower()
+
+    if strength == "weak":
+        length = 5
+    elif strength == "strong":
+        length = 8
+    elif strength == "very strong":
+        length = 12
+    else:
+        print("Invalid input. Please enter weak, strong, or very strong.")
+        return
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(secrets.choice(characters) for _ in range(length)) #star of the day
+    print(f"Your {strength} password is: {password}")
+
+    generate_password()
